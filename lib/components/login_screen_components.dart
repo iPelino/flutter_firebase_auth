f155e284000f3a5bef0a44ef19fe5c689123926f
Pelin Mutanguha <pelmut2000@gmail.com>
2021-11-17 23:49:14 +0200

    Refactor login components

--- lib/components/login_screen_components.dart
+++ lib/components/login_screen_components.dart
@@ -2,6 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/constants/colors.dart';
 import 'package:flutter_firebase_auth/screens/register_screen.dart';
 
+final _formKey = GlobalKey<FormState>();
+
+final TextEditingController emailController = new TextEditingController();
+final TextEditingController passwordController = new TextEditingController();
+
+TextFormField emailTextFormField() {
+  return TextFormField(
+    autofocus: false,
+    controller: emailController,
+    keyboardType: TextInputType.emailAddress,
+    onSaved: (value) {
+      emailController.text = value!;
+    },
+    textInputAction: TextInputAction.next,
+    style: TextStyle(
+      color: grey_ish,
+    ),
+    decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.mail,
+          color: grey_ish,
+          semanticLabel: "Email",
+        ),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "username@example.com",
+        hintStyle: TextStyle(color: grey_ish),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: grey_ish),
+        )),
+  );
+}
+
+TextFormField passwordTextFormField() {
+  return TextFormField(
+    autofocus: false,
+    obscureText: true,
+    controller: passwordController,
+    onSaved: (value) {
+      emailController.text = value!;
+    },
+    textInputAction: TextInputAction.done,
+    style: TextStyle(
+      color: grey_ish,
+    ),
+    decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.lock,
+          color: grey_ish,
+          semanticLabel: "Password",
+        ),
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        hintText: "Password",
+        hintStyle: TextStyle(color: grey_ish),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: grey_ish),
+        )),
+  );
+}
+
 ElevatedButton registerElevatedButton(BuildContext context) {
   return ElevatedButton(
     onPressed: () {
@@ -43,6 +104,73 @@ ElevatedButton loginElevatedButton() {
       foregroundColor: MaterialStateProperty.all(black_ish),
 
       // onPrimary: Colors.white38,
+    ),
+  );
+}
+
+Form loginForm(TextFormField emailField, TextFormField passwordField,
+    ElevatedButton loginButton, ElevatedButton registerButton) {
+  return Form(
+    key: _formKey,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 120,
+          child: Image.asset(
+            "assets/images/student.png",
+            fit: BoxFit.contain,
+          ),
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        emailField,
+        SizedBox(
+          height: 15,
+        ),
+        passwordField,
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => RegisterScreen(),
+                //   ),
+                // );
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(left: 5.0),
+                child: Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: grey_ish,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            loginButton,
+            registerButton,
+          ],
+        ),
+      ],
     ),
   );
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,64 +10,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-
-  final TextEditingController emailController = new TextEditingController();
-  final TextEditingController passwordController = new TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    final emailField = TextFormField(
-      autofocus: false,
-      controller: emailController,
-      keyboardType: TextInputType.emailAddress,
-      onSaved: (value) {
-        emailController.text = value!;
-      },
-      textInputAction: TextInputAction.next,
-      style: TextStyle(
-        color: grey_ish,
-      ),
-      decoration: InputDecoration(
-          prefixIcon: Icon(
-            Icons.mail,
-            color: grey_ish,
-            semanticLabel: "Email",
-          ),
-          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          hintText: "username@example.com",
-          hintStyle: TextStyle(color: grey_ish),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: grey_ish),
-          )),
-    );
+    final emailField = emailTextFormField();
 
-    final passwordField = TextFormField(
-      autofocus: false,
-      obscureText: true,
-      controller: passwordController,
-      onSaved: (value) {
-        emailController.text = value!;
-      },
-      textInputAction: TextInputAction.done,
-      style: TextStyle(
-        color: grey_ish,
-      ),
-      decoration: InputDecoration(
-          prefixIcon: Icon(
-            Icons.lock,
-            color: grey_ish,
-            semanticLabel: "Password",
-          ),
-          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          hintText: "Password",
-          hintStyle: TextStyle(color: grey_ish),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: grey_ish),
-          )),
-    );
+    final passwordField = passwordTextFormField();
 
     final loginButton = loginElevatedButton();
     final registerButton = registerElevatedButton(context);
@@ -80,69 +27,8 @@ class _LoginScreenState extends State<LoginScreen> {
             color: black_ish,
             child: Padding(
               padding: const EdgeInsets.all(32.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 120,
-                      child: Image.asset(
-                        "assets/images/student.png",
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 50,
-                    ),
-                    emailField,
-                    SizedBox(
-                      height: 15,
-                    ),
-                    passwordField,
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(
-                            //     builder: (context) => RegisterScreen(),
-                            //   ),
-                            // );
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 5.0),
-                            child: Text(
-                              "Forgot Password?",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14,
-                                color: grey_ish,
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        loginButton,
-                        registerButton,
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+              child: loginForm(
+                  emailField, passwordField, loginButton, registerButton),
             ),
           ),
         ),
